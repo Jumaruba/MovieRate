b60a9cca5dc2d501fc8753bafab1030e7caf9b70
Juliane Marubayshi <juliane.marubayashi@gmail.com>
2021-02-14 12:15:33 +0000

    promote moviesTable to clas

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,50 +1,52 @@
-import React from 'react';
-import Like from './Like'; 
+import React, { Component } from 'react';
+import Like from './Like';
 
-const MoviesTable = (props) => {
-    const { movies, onDelete, onLike, onSort } = props;
-    
-    if (movies.length === 0 ) return (<p> No movies to display!</p>)
 
-    return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th onClick={() => onSort('title')}>Title</th>
-                    <th onClick={() => onSort('genre.name')}>Genre</th>
-                    <th onClick={() => onSort('numberInStock')}>In Stock</th>
-                    <th onClick={() => onSort('dailyRentalRate')}>Rate</th>
-                    <th> </th>
-                    <th> </th>
-                </tr>
+class MoviesTable extends Component {
+    render() {
+        const { movies, onDelete, onLike, onSort } = this.props;
 
-            </thead>
-            <tbody>
-                {movies.map(movie => (
-                    <tr key={movie._id}>
-                        <td>{movie.title}</td>
-                        <td>{movie.genre.name}</td>
-                        <td>{movie.numberInStock}</td>
-                        <td>{movie.dailyRentalRate}</td>
-                        <td> <Like
-                            liked={movie.liked}
-                            onClick={() => onLike(movie)} /> </td>
-                        <td>
-                            <button
-                                type="button"
-                                className="btn btn-danger"
-                                onClick={() => onDelete(movie)}
-                            >
-                                Delete
-                            </button>
-                        </td>
+        if (movies.length === 0) return (<p> No movies to display!</p>)
+
+        return (
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th onClick={() => onSort('title')}>Title</th>
+                        <th onClick={() => onSort('genre.name')}>Genre</th>
+                        <th onClick={() => onSort('numberInStock')}>In Stock</th>
+                        <th onClick={() => onSort('dailyRentalRate')}>Rate</th>
+                        <th> </th>
+                        <th> </th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
-    );
-}
 
+                </thead>
+                <tbody>
+                    {movies.map(movie => (
+                        <tr key={movie._id}>
+                            <td>{movie.title}</td>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                            <td> <Like
+                                liked={movie.liked}
+                                onClick={() => onLike(movie)} /> </td>
+                            <td>
+                                <button
+                                    type="button"
+                                    className="btn btn-danger"
+                                    onClick={() => onDelete(movie)}
+                                >
+                                    Delete
+                            </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        );
+    }
+}
 
 
 export default MoviesTable;
